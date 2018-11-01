c1326ec8f0743af2f707b42a762b16fe193dd8b2
Chris Roche <chris.roche@yousoft.com>
2018-11-01 16:08:24 +0000

    Extra controllers/legacy API example

--- WebAPITutorial/App_Start/WebApiConfig.cs
+++ WebAPITutorial/App_Start/WebApiConfig.cs
@@ -14,11 +14,20 @@ namespace WebAPITutorial
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            // For eg. Legacy Api methods, redirecting to the new ones
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                name: "ProdAPI",
+                routeTemplate: "api/prod/{id}",
+                defaults: new { controller = "products", id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+               name: "DefaultApi",
+               routeTemplate: "api/{controller}/{id}",
+               defaults: new { id = RouteParameter.Optional }
+           );
+
+
         }
     }
 }
